054ff3f1ed5bd29815e8500f9122aa19c89d3a29
Sumat Mallick <115460605+SumatM@users.noreply.github.com>
2023-07-05 19:40:53 +0530

    use state in next.js

--- sprint3/day3/next-todo/src/app/page.js
+++ sprint3/day3/next-todo/src/app/page.js
@@ -1,32 +1,39 @@
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+
+  function handleAdd(){
+    setCount((count)=> count+1)
+  }
+  function handleReduce(){
+    if(count==0){
+      return alert('Count is at Minimum 0')
+    }
+    setCount((count)=>count-1)
+  }
   return (
-    <main >
-      <h1 className="text-center text-2xl font-bold">Todo App</h1>
-      <MyInfo  name={"sumat"}/>
-      <HisInfo name={'aman'}/>
+    <main>
+      <h1 className="text-center text-2xl font-bold">Counter App</h1>
+      <div>
+        <h2 className="text-center font-bold text-4xl my-7"> Count: {count}</h2>
+      </div>
+      <div className="text-center">
+          <button
+            className="border border-black  px-4 py-2 mr-2"
+            onClick={handleReduce}
+          >
+            -
+          </button>
+
+          <button
+            className="border border-black  px-4 py-2 ml-2"
+            onClick={handleAdd}
+          >
+            +
+          </button>
+        </div>
     </main>
-  )
+  );
 }
-
-
-
-export const MyInfo = (prop)=>{
-
-  return (
-    <div>
-      Hello! my name if {prop.name}
-    </div>
-  )
-}
-
-
-export const HisInfo = ({name})=>{
-  console.log(name);
-  return (
-    <div>
-      Hello! my name if {name}
-    </div>
-  )
-}
-
