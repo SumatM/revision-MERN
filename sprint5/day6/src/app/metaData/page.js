b080e6c6712964d8c58d0399093af02b2a14503e
Sumat Mallick <115460605+SumatM@users.noreply.github.com>
2023-07-26 12:47:07 +0530

    metadata

---
+++ sprint5/day6/src/app/metaData/page.js
@@ -0,0 +1,18 @@
+export default function metaData() {
+  return (
+    <div>
+      <h1>Meta Data</h1>
+    </div>
+  );
+}
+
+
+
+export function generateMetadata(){
+    
+    return({
+        title:" Meta Data Test",
+        description:"This is demo page",
+        robots:'index , fellow'
+    })
+}
